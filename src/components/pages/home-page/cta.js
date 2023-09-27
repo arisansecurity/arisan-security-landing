26a15d63e8881b423b8205f8cd94f2ab1eff5eba
Gilang Windu Asmara <gilangwinduasmara2@gmail.com>
2023-09-27 21:29:40 +0700

    fix(cta): cleanse design

--- src/components/pages/home-page/cta.js
+++ src/components/pages/home-page/cta.js
@@ -2,11 +2,11 @@ import Button from "../../button";
 
 export default function Cta() {
     return (
-        <section className="min-h-screen px-8 md:px-32 bg-gray-100 flex items-center snap-start">
+        <section className="min-h-screen px-8 md:px-32 bg-gray-100 flex items-center snap-start relative">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-24" >
-                <div className="h-96 rounded-xl bg-blue-500">
+                {/*<div className="h-96 rounded-xl bg-blue-500">*/}
 
-                </div>
+                {/*</div>*/}
                 <div>
                     <div className="pb-10">
                         <p className="text-4xl lg:text-3xl leading-none font-bold text-gray-800 tracking-tight mb-8">Menulis Adalah Jendela Ilmu, dari Segala Ilmu</p>
@@ -14,10 +14,9 @@ export default function Cta() {
                         <div className="text-gray-600 leading-6 text-justify font-light">
                             <p>Kami menerapkan sistem model Crowdsourcing yang berarti, siapapun bisa berkontribusi kepada platform ini. Dari komunitas untuk komunitas, dari kamu untuk kamu. Setiap tulisan kamu akan dilakukan proses proofreading oleh Redaktur.</p>
                             <p>Jika tulisan kamu sudah layak memenuhi syarat untuk terbit, kamu akan mendapatkan poin yang berguna untuk memimpin papan Jawara Cendekia layaknya Hall Of Fame.</p>
-                            <p>Kirimkan tulisan kamu sekarang juga dengan menekan tombol “KIRIM TULISAN” di samping.</p>
                         </div>
                     </div>
-                    <div className="flex" style={{ backgroundImage: "url('/images/tulis-bg.png')", backgroundSize: 'contain' }}>
+                    <div className="flex">
                         <a href="http://blog.arisansecurity.id" target="_blank">
                             <Button className="px-20 rounded-sm">Kirim Tulisan</Button>
                         </a>
